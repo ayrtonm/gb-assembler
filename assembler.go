f03b29630cb9da67d5e886bb31dab93b21ddfc47
Ayrton <a.munoz3327@gmail.com>
2019-06-26 10:27:00 -0400

    binaries now have cartridge header info
    added bitmap of the logo displayed at startup, added the header checksum and
    made the file size the correct size by filling in the end with zeros

--- assembler.go
+++ assembler.go
@@ -31,6 +31,9 @@ const ptrSuffix string = "]"
 const numTitleChars int = 16
 const titleAddress uint16 = 0x134
 const startAddress uint16 = 0x0100
+const logoAddress uint16 = 0x0104
+const checksumAddress uint16 = 0x014D
+var nintendoLogo []byte = []byte{0xCE, 0xED, 0x66, 0x66, 0xCC, 0x0D, 0x00, 0x0B, 0x03, 0x73, 0x00, 0x83, 0x00, 0x0C, 0x00, 0x0D, 0x00, 0x08, 0x11, 0x1F, 0x88, 0x89, 0x00, 0x0E, 0xDC, 0xCC, 0x6E, 0xE6, 0xDD, 0xDD, 0xD9, 0x99, 0xBB, 0xBB, 0x67, 0x63, 0x6E, 0x0E, 0xEC, 0xCC, 0xDD, 0xDC, 0x99, 0x9F, 0xBB, 0xB9, 0x33, 0x3E}
 
 //offset pattern used in inc/dec for 16 bit registers
 var regOffsets1 = map[string]byte{"bc":0, "de":1, "hl":2, "sp":3}
@@ -381,5 +384,25 @@ func main() {
       break
     }
   }
+  outfile.Seek(int64(logoAddress), 0)
+  _, err = outfile.Write(nintendoLogo)
+
+  outfile.Seek(int64(titleAddress), 0)
+  checksum := []byte{0}
+  temp := []byte{0}
+  for i := titleAddress; i < checksumAddress; i++ {
+    outfile.Read(temp)
+    checksum[0] -= (1 + temp[0])
+  }
+  outfile.Seek(int64(checksumAddress), 0)
+  _, err = outfile.Write(checksum)
+  check(err)
+
+  //fill in rest of the file with zeros
+  outfile.Seek(0,2)
+  var fill []byte = make([]byte,0x8000)
+  _, err = outfile.Write(fill)
+  check(err)
+  outfile.Truncate(0x8000)
   outfile.Sync()
 }
