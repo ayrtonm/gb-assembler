f821946b03f2b72df5bc7758ffc27253df03def5
Ayrton <a.munoz3327@gmail.com>
2019-12-02 20:58:20 -0600

    added won_point function

--- points.asm
+++ points.asm
@@ -12,25 +12,52 @@ lost_point:
   jpz update_oam
   //get pointer to the right place in the OAM
   move_pointer:
-    inc $hl
-    inc $hl
-    inc $hl
-    inc $hl
-    dec $b
-    dec $b
-    dec $b
-    dec $b
-    dec $b
-    dec $b
-    dec $b
-    dec $b
+    ld $a $l
+    add 4
+    ld $l $a
+    ld $a $b
+    sub 8
+    ld $b $a
     dec $c
     jpnz move_pointer
   update_oam:
     ld $a $b
     ld [hl] $a
+    call long_wait
+  //check for game over
+  ld $a [losses]
+  sub 3
+  jpz setup
   jp reset_game
 
 won_point:
+  //increase losses variable
+  ld $a [wins]
+  inc $a
+  ld [wins] $a
+  //draw point on the screen
+  ld $hl point1_x
+  ld $b 8
+  ld $a [wins]
+  ld $c $a
+  dec $c
+  jpz update_oam
+  //get pointer to the right place in the OAM
+  move_pointer:
+    ld $a $l
+    add 4
+    ld $l $a
+    ld $a $b
+    add 8
+    ld $b $a
+    dec $c
+    jpnz move_pointer
+  update_oam:
+    ld $a $b
+    ld [hl] $a
+    call long_wait
+  //check for game over
+  ld $a [wins]
+  sub 3
+  jpz setup
   jp reset_game
-
